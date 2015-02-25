f4c24b997ffd44cc49b123252d8b7093d5936a62
Baptiste Pernet <bapt@sp4ce.net>
2015-02-25 14:06:49 -0500

    Clean code

--- www/sendmail.js
+++ www/sendmail.js
@@ -1,20 +1,20 @@
 var sendmail = {
-    
-		send: function(successCallback, errorCallback, subject, body, sender, password, recipients){
-			cordova.exec(successCallback,
-			    errorCallback,
-			    "SendMail",
-			    "send",
-			    [{
-			     	"subject":subject, 
-			       	"body":body,
-			       	"sender":sender,
-			       	"password":password, 
-			       	"recipients":recipients
-			    }]
-			);
-			
-		}
+
+    send: function(successCallback, errorCallback, subject, body, sender, password, recipients){
+        cordova.exec(successCallback,
+            errorCallback,
+            "SendMail",
+            "send",
+            [{
+                 "subject":subject,
+                 "body":body,
+                 "sender":sender,
+                 "password":password,
+                 "recipients":recipients
+            }]
+        );
+
+    }
 }
 
 module.exports = sendmail;
